917c227ce7c8d687d139d4aff14e8acc835bcf3d
UshasriGajal <126794491+UshasriGajal@users.noreply.github.com>
2023-06-15 16:28:42 +0530

    Update newpage.js

--- newpage.js
+++ newpage.js
@@ -1,8 +1,8 @@
 function run(){
     if(sessionStorage.getItem("user")=="usha"){
-        window.location.href="login_page2.html"
+        window.location.href="index2.html"
     }
     else{
-        window.location.href="login_pagerrr.html"
+        window.location.href="error.html"
     }
 }
